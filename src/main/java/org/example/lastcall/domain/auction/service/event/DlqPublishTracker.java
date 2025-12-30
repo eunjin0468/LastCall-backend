e9fef8545a2a3dde22b6b2b5ab96061a50018d04
dxxwls <eunjin0468@naver.com>
2025-12-30 21:48:08 +0900

    refactor: 사용하지 않는 import문 제거

--- src/main/java/org/example/lastcall/domain/auction/service/event/DlqPublishTracker.java
+++ src/main/java/org/example/lastcall/domain/auction/service/event/DlqPublishTracker.java
@@ -2,18 +2,15 @@ package org.example.lastcall.domain.auction.service.event;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DlqPublishTracker {
 
-  private final Cache<String, Boolean> returnedCorrelationIds =
-      Caffeine.newBuilder()
-          .expireAfterWrite(10, TimeUnit.SECONDS)  // 10초 후 자동 삭제
-          .build();
+  private final Cache<String, Boolean> returnedCorrelationIds = Caffeine.newBuilder()
+      .expireAfterWrite(10, TimeUnit.SECONDS)  // 10초 후 자동 삭제
+      .build();
 
   public Cache<String, Boolean> getReturnedCorrelationIds() {
     return returnedCorrelationIds;
